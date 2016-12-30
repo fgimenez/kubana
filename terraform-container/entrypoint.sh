197261cfa3fadc2740b64fdc544152ffef6134c1
Federico Gimenez <fgimenez@coit.es>
2016-12-30 11:11:41 +0100

    default command

--- terraform-container/entrypoint.sh
+++ terraform-container/entrypoint.sh
@@ -2,4 +2,10 @@
 
 cd /app || exit 1
 
-terraform apply -var "username=$OS_USERNAME" -var "password=$OS_PASSWORD" -var "tenant=$OS_TENANT_NAME" -var "auth_url=$OS_AUTH_URL"
+if [ -z "$1" ]; then
+    cmd=apply
+else
+    cmd="$1"
+fi
+
+terraform "$cmd" -var "username=$OS_USERNAME" -var "password=$OS_PASSWORD" -var "tenant=$OS_TENANT_NAME" -var "auth_url=$OS_AUTH_URL"
